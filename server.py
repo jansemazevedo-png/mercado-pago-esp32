e7bae510235fcd28fb9f946961b47a551c454ff1
jansemazevedo-png <jansemazevedo@gmail.com>
2026-09-06 10:49:59 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-```python
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -67,4 +67,4 @@ def inicio():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-```
+
